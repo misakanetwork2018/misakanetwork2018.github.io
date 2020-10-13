63d232e0a7caafc438f40f4d6b6985d4737f8fbd
misakanetwork2018 <misakanetwork2018@gmail.com>
2020-10-13 16:59:35 +0800

    disable telegram login

--- statics/config.js
+++ statics/config.js
@@ -1,7 +1,8 @@
 window.appConfig = {
   site: 'github',
   apiUrl: 'https://api.msknw.live',
-  telegramBotName: 'misakanetwork_github_bot',
+  enableTelegramLogin: false,
+  telegramBotName: 'misakanetwork_bot',
   recaptcha: {
     v2_sitekey: '6Lcku9YZAAAAAA8iyiCNw9xtE5D51Bp5m5GzmOhp',
     v3_sitekey: '6LeFutYZAAAAABKEta8nJrKd80WU7q1jwIGcd4rd'
