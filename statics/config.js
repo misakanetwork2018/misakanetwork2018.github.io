10d13a005d5f5d23dd4a8dfd88bb73f0c8b728ac
misakanetwork2018 <misakanetwork2018@gmail.com>
2020-10-13 15:07:14 +0800

    first commit

---
+++ statics/config.js
@@ -0,0 +1,12 @@
+window.appConfig = {
+  site: 'github',
+  apiUrl: 'https://api.msknw.live',
+  telegramBotName: 'misakanetwork_github_bot',
+  recaptcha: {
+    v2_sitekey: '6Lcku9YZAAAAAA8iyiCNw9xtE5D51Bp5m5GzmOhp',
+    v3_sitekey: '6LeFutYZAAAAABKEta8nJrKd80WU7q1jwIGcd4rd'
+  },
+  payments: {
+    alipay: '支付宝'
+  }
+}
