571bb63d43a3dee9dc6ff5c55a7a082637f1e966
misakanetwork2018 <misakanetwork2018@gmail.com>
2020-12-30 15:42:32 +0800

    update

--- statics/config.js
+++ statics/config.js
@@ -1,7 +1,7 @@
 window.appConfig = {
   site: 'github',
-  apiUrl: 'https://api.msknw.live',
-  enableTelegramLogin: false,
+  apiUrl: 'https://api.msknw.xyz',
+  enableTelegramLogin: true,
   telegramBotName: 'misakanetwork_bot',
   recaptcha: {
     v2_sitekey: '6Lcku9YZAAAAAA8iyiCNw9xtE5D51Bp5m5GzmOhp',
