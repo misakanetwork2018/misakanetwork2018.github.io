72741dcfef86e56cb11ddfd1ecc8b815ba7edc23
misakanetwork2018 <misakanetwork2018@gmail.com>
2021-11-26 02:51:19 +0800

    fixed config

--- statics/config.js
+++ statics/config.js
@@ -1,6 +1,6 @@
 window.appConfig = {
   site: 'github',
-  apiUrl: 'https://api.msknw.xyz',
+  apiUrl: 'https://api.msknw.com',
   recaptcha: '6Lcku9YZAAAAAA8iyiCNw9xtE5D51Bp5m5GzmOhp',
   bbUrl: 'https://ann.msknw.com'
 }
