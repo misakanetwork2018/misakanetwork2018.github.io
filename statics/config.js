084bed3318b2070887f67af74162c106dd4faf79
misakanetwork2018 <misakanetwork2018@gmail.com>
2021-11-26 02:43:42 +0800

    update

--- statics/config.js
+++ statics/config.js
@@ -1,13 +1,6 @@
 window.appConfig = {
   site: 'github',
   apiUrl: 'https://api.msknw.xyz',
-  enableTelegramLogin: true,
-  telegramBotName: 'misakanetwork_bot',
-  recaptcha: {
-    v2_sitekey: '6Lcku9YZAAAAAA8iyiCNw9xtE5D51Bp5m5GzmOhp',
-    v3_sitekey: '6LeFutYZAAAAABKEta8nJrKd80WU7q1jwIGcd4rd'
-  },
-  payments: {
-    alipay: '支付宝'
-  }
+  recaptcha: '6Lcku9YZAAAAAA8iyiCNw9xtE5D51Bp5m5GzmOhp',
+  bbUrl: 'https://ann.msknw.com'
 }
